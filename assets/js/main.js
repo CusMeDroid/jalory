7571d22e1954fe7a8e7559b692cd182aeb3d924d
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-05-01 10:21:46 +0700

    Add files via upload
    Signed-off-by: CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,13 @@
+/* Search Function */
+function searchFunc() {
+    var mSearch = document.getElementById("txtsearch").value;
+    if (mSearch < 1 || !mSearch) {
+        alert("Do not be empty!");
+    } else {
+        alert("Coming Soon!");
+    }
+}
+
 /* Nav button */
 document.getElementById("n_home").onclick = function () {
     document.getElementById("n_home").setAttribute("class", "a_active");
@@ -143,7 +153,7 @@ function myFunction(x) {
             /* Hide */
             document.getElementById("myprofile").style.display = "none";
             document.getElementById("btnxprofile").style.display = "none";
-            document.getElementById("btnprofile").style.display = "block"; 
+            document.getElementById("btnprofile").style.display = "block";
         };
 
         document.getElementById("btnprofile").onclick = function () {
@@ -169,3 +179,57 @@ myFunction(x);
 x.addEventListener("change", function() {
   myFunction(x);
 });
+
+/* Disclaimer */
+var myUrl = ""; // this add your url
+var cpr = document.getElementById("myCopyright");
+if(!cpr){
+    window.location.href = myUrl;
+}
+
+function redirectCU(e) {
+    if (e.ctrlKey && e.which == 85) {
+        window.location.replace(myUrl);
+        return false;
+    }
+}
+
+document.onkeydown = redirectCU;
+
+function redirectKK(e) {
+    if (e.which == 3) {
+        alert("Thank you for visiting my website :)");
+        return false;
+    }
+}
+
+document.oncontextmenu = redirectKK;
+
+document.addEventListener("keyup", (e) => {
+    if (e.key == "PrintScreen") {
+        navigator.clipboard.writeText("");
+        alert("Screenshots disabled!");
+    }
+});
+
+document.addEventListener("keydown", (e) => {
+    if (e.ctrlKey && e.key == "p") {
+        alert("This section is not allowed to print or export to PDF");
+        e.cancelBubble = true;
+        e.preventDefault();
+        e.stopImmediatePropagation();
+    }
+});
+
+const unique = "cusmedroid.github.io";
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("cpr").innerHTML = year;
+
+if (window.location.hostname == unique) {
+  // alert("Sukses");
+} else {
+  alert("Buy source");
+  window.location.href = "https://api.whatsapp.com/send?phone=6285772757932&text=Hello,%20I%20Need%20Source%20Code%20Hot%20News%20Template.";
+}
